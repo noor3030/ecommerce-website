fc7f99c21c18aeb98602ed18c5d44b177080ffa9
noor3030 <nnorkrka@gmail.com>
2024-11-18 12:09:31 +0300

    edit home categories

--- ecommerce-website/src/components/HomeCategories.vue
+++ ecommerce-website/src/components/HomeCategories.vue
@@ -29,26 +29,22 @@ onMounted(async () => {
     <h1 class="dark:text-white text-xl text-center">
       {{ $t("Shop Our Top Categories") }}
     </h1>
-    <div class="flex flex-col space-y-4 justify-center content-center">
+    <div class="grid grid-cols-2  gap-y-5 justify-items-center place-items-center" >
       <router-link
         v-for="category in categoriesList"
         :key="category.id"
         :to="`/category/${category.id}`"
-        class="flex flex-col space-y-2"
+        class="flex flex-col w-11/12"
       >
-      <div class="space-y-2">
-        <img
-          :src="category.image"
-          alt=""
-          class="rounded-xl self-center w-full"
-        />
-        <h2 class="dark:text-white text-xl text-center rtl:hidden">
-          {{ category.name }}
-        </h2>
-        <h2 class="dark:text-white text-xl text-center hidden rtl:block">
-          {{ category.arName }}
-        </h2>
-      </div>
+        <div class="space-y-1">
+          <img :src="category.image" alt="" class="rounded-xl" />
+          <h2 class="dark:text-white text-xl text-center rtl:hidden">
+            {{ category.name }}
+          </h2>
+          <h2 class="dark:text-white text-xl text-center hidden rtl:block">
+            {{ category.arName }}
+          </h2>
+        </div>
       </router-link>
     </div>
   </section>
